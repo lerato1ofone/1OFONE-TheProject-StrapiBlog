74f34493e738de19b897ece348ccc4b5bf0a8c65
Lerato Letsepe <LeratoL@ragingriver.co.za>
2023-10-06 19:38:52 +0200

    06102023 Release trigger

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 8082),
-  admin: {
+  admin: { 
     auth: {
       secret: env("ADMIN_JWT_SECRET", "773b9fe92c18413313b726c988728311"),
     },
